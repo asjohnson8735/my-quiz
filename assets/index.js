4d30f1ddddc3697af9482c48f1202ab40f5b959b
asjohnson8735 <asjohnson8735@gmail.com>
2023-04-14 00:05:28 -0500

    updated content for blueprint

--- assets/index.js
+++ assets/index.js
@@ -11,11 +11,20 @@
 // THEN I can save my initials and score
 
 const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById('question-container'
-)
-const shuffledQuestions, currentQuestionIndex
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+
+let shuffledQuestions, currentQuestionIndex
+
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
 startButton.addEventListener('click', startGame)
+
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
     console.log('Started')
@@ -27,16 +36,74 @@ function startGame() {
 }
 
 function setNextQuestion() {
-
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 
 
 }
 
+function showQuestion(question) {
+
+    questionElement.innerText = question.question
+    question.answer.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+    });
+}
+
+function resetState() {
+clearStatusClass(document.body)
+nextButton.classList.add('hide')
+while (answerButtonsElement) {
+    answerButtonsElement.removeChild
+    (answerButtonsElement.firstChild)
+}
+
+function selectAnswer(e) {
+
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    }
+  )
+  if (shuffledQuestions.length > currentQuestionIndex + 1 ) {
+  nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+
+function setStatusClass(element, correct) {
+
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+}
+
+function clearStatusClass(element) {
+
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
 const questions = [
 
     {
-        question: 'What is 2+2'
+        question: 'What is 2+2' ,
         answers: [
             [
                 { text: '4', correct: true},
